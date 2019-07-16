51dbcf52fd4ae7b0b94557899c3e499eefb1bd96
Sophie OT <SophieOT@Sophies-MacBook-Pro.local>
2019-07-15 20:53:45 -0700

    trying

--- server.js
+++ server.js
@@ -7,3 +7,13 @@ var exphbs  = require('express-handlebars');
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://<user1>:<password1>@ds155864.mlab.com:55864/heroku_298hmhcd";
 
 mongoose.connect(MONGODB_URI);
+
+var app = express();
+
+app.get("/", function(req,res) {
+    res.send("he;lwo wokrcd");
+});
+
+app.listen(3000, function() {
+    console.log("app runnin on 3000");
+});
