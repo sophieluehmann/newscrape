2165c8cdbf10ecb7d40975d3837b9701266110f3
Sophie OT <SophieOT@Sophies-MacBook-Pro.local>
2019-07-15 20:31:23 -0700

    database connect

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ var express = require("express");
 var axios = requie("axios");
 var mongoose = require("mongoose");
 var exphbs  = require('express-handlebars');
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://<user1>:<password1>@ds155864.mlab.com:55864/heroku_298hmhcd";
+
+mongoose.connect(MONGODB_URI);
