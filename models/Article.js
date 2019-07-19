c3bd5fe6242e67f6872f4e39fc52a481dc3da36c
Sophie OT <SophieOT@D-10-19-81-172.dhcp4.washington.edu>
2019-07-18 18:49:05 -0700

    scraping is working, handlebars rendering, very ugly

--- models/Article.js
+++ models/Article.js
@@ -2,13 +2,24 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var TestSchema = new Schema({
+
+var ArticleSchema = new Schema({
     title: {
         type: String,
-        require: true
-    }
+        require: true,
+        unique: true,
+        dropUps: true,
+        useCreateIndex: true
+    },
+    link: {
+        type: String,
+        require: true,
+        unique: true,
+        dropUps: true,
+        useCreateIndex: true
+    },
 });
 
-var Article = mongoose.model("Article", TestSchema);
+var Article = mongoose.model("Article", ArticleSchema);
 
 module.exports = Article;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var cheerio = require("cheerio");
 var express = require("express");
 var axios = require("axios");
 var mongoose = require("mongoose");
+var methodOverride = require("method-override");
 var exphbs  = require('express-handlebars');
 
 var Article = require("./models/Article.js");
@@ -17,48 +18,89 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+app.use(methodOverride('_method'));
+
+app.set('views', __dirname + '/views');
+app.engine("handlebars", exphbs({ defaultLayout: "main", layoutsDir: __dirname + "/views/layouts" }));
+app.set("view engine", "handlebars");
+
 var db = mongoose.connection;
 
 db.once("open", function() {
 	console.log("Mongoose connection successful.");
 });
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname + "./public/index.html"));
-  });
+
+
+app.get("/", function (req, res) {
+  Article.find({})
+    .exec(function (error, data) {
+      if (error) {
+        res.send(error);
+      }
+      else {
+        var news = {
+          Article: data
+        };
+        //res.send(news);
+        res.render("index", news);
+      }
+    });
+});
+  
 
   app.get("/scrape", function(req, res) {
-    // Make a request via axios for the news section of `ycombinator`
     axios.get("https://news.sky.com/strangenews").then(function(response) {
       var $ = cheerio.load(response.data);
+      
       $(".sdc-site-tile__body-main").each(function(i, element) {
 
         var result = {};
 
         result.title = $("body > div.sdc-site-tiles > div > div > div:nth-child(" + i + ") > div > div > h3 > a > span").text();
-        
-        if (result.title) {
+        result.link = "https://news.sky.com/" +  $("body > div.sdc-site-tiles > div > div > div:nth-child(" + i + ") > div > div > h3 > a").attr("href");
+
+        if (result.title && result.link) {
         var newArticle = new Article(result);
 
-        newArticle.save(function(err, inserted) {
+        newArticle.save(function(err, doc) {
           if (err) {
             console.log(err);
           } else {
-            console.log(inserted);
+            console.log(doc);
           }
         });
       };
+       
       });
-    
-     
+      /*$(".sdc-site-tile__body-main").each(function(i, element) {
+        console.log(i);
+        var result = {};
+        
+        result.title = $("#load-more-list > div > div > div > div:nth-child(" + i + ") > div > div > h3 > a > span").text();
+        result.link = "https://news.sky.com/" +  $("#load-more-list > div > div > div > div:nth-child(" + i + ") > div > div > h3 > a > span").attr("href");
+
+        if (result.title && result.link) {
+        var newArticle = new Article(result);
+
+        newArticle.save(function(err, doc) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(doc);
+          }
+        });
+      };
+       
+      });
+
+    */
+      res.redirect("/");
 
     });
-  
-    
 
-  
-    // Send a "Scrape Complete" message to the browser
-    res.send("Scrape Complete");
+    
+    
   });
   
 
