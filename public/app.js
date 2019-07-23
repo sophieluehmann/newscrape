ca64c58b9c53ffe0679681ebf9a3ace1c28d70f6
Sophie OT <SophieOT@Sophies-MacBook-Pro.local>
2019-07-23 16:31:23 -0700

    added 'save' ajax, changed style stuff

--- public/app.js
+++ public/app.js
@@ -1,6 +1,3 @@
-
-  
- 
 $(document).ready(function() {
 
 
@@ -8,6 +5,19 @@ $(document).ready(function() {
 
 console.log("app js working");
 
-
+$(".btn").on("click", function(event) {
+    var link = $(this.children).attr("link");
+    $.ajax({
+        url: "/save",
+        type: "POST",
+        data: link
+    }).then(
+        function(link){
+            console.log("ajax complete")
+        }
+    );
+    console.log("ajax sent");
+    
+});
 
 }); 
